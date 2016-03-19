481e1c2d6eafae47a4117c810e87a578f2c1d10f
zbay <zlwilliams1@gmail.com>
2016-03-19 18:50:06 +0000

    Updated to React 0.14.7

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var routes = require('./controllers');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 var mongo = require('mongodb');
-var session = require('client-sessions');
 
 mongoose.connect('mongodb://localhost:27017/opine', function (err, db)
 //mongoose.connect(process.env.MONGOLAB_URI, function (err, db)
--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,5 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Routes = require('./routes');
 
-React.render(Routes, document.querySelector('.container'));
-
-/*var React = require('react');
-var Home = require('./home');
-
-var element = React.createElement(Home);
-React.render(element, document.querySelector('.container'));*/
+ReactDOM.render(Routes, document.querySelector('.container'));
