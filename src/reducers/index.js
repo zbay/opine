95025a24e45deabea4d5ac453dc7a9a6ef0ea6a4
zbay <zlwilliams1@gmail.com>
2016-03-22 02:53:48 +0000

    Removed redux stuff for now

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import PostsReducer from './reducer_posts';
-
-const rootReducer = combineReducers({
-  posts: PostsReducer
-});
-
-export default rootReducer;
--- src/reducers/reducer_posts.js
+++
@@ -1,9 +0,0 @@
-import { FETCH_POSTS } from '../actions/index';
-
-export default function(state = [], action) {
-  switch (action.type) {
-  case FETCH_POSTS:
-    return [ action.payload.data, ...state ];
-  }
-  return state;
-}
