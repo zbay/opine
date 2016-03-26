0dcbf49860a07bf9e7dfd73753a0d3498edadabb
zbay <zlwilliams1@gmail.com>
2016-03-26 23:48:26 +0000

    IP address is now saved for question posters

--- controllers/addPost.js
+++ controllers/addPost.js
@@ -8,7 +8,8 @@ module.exports = function(app) {
     if(!req.body.question || !req.body.asker || !req.body.contact || !req.body.deadline || !req.body.category){
         res.json({"error": "Please fill out the entire form."});
     }
-    var newPost = new Posting({"question": req.body.question, "asker": req.body.asker, "howToContact": req.body.contact, "deadline": req.body.deadline, "category": req.body.category});
+    var newPost = new Posting({"question": req.body.question, "asker": req.body.asker, "howToContact": req.body.contact,
+    "deadline": req.body.deadline, "category": req.body.category, "IP": req.body.IP});
     newPost.save(function(err, msg){
           res.json({"success": msg}); 
     });
--- dbmodels/posting.js
+++ dbmodels/posting.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var PostingSchema = new Schema({"question": {type: String}, "timePosted": {type: Date, required: true, default: Date.now}, "deadline": {type: Date, required: true}, 
 "howToContact": {type: String, required: true}, "category": {type: String, required: true}, "asker": {type: String, required: true}, "IP": String, 
-"comments": [{"text": {type: String, required: true}, "timePosted": {type: Date, required: true, default: Date.now}, "IP": String}]
+"comments": [{"text": {type: String, required: true}, "timePosted": {type: Date, required: true, default: Date.now}, "IP": {type: String, required: true}}]
 });
 
 PostingSchema.index({ question: "text", howToContact: "text", asker: "text"});
