51ccbdf370605486e8c9a0a0648efb8bd0553430
zbay <zlwilliams1@gmail.com>
2016-03-27 17:54:57 +0000

    React Router now accomodates URLs lacking page numbers

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -13,19 +13,19 @@ var BadLink = require("./components/StatelessContent/BadLink");
 
 var AllWrapper = React.createClass({
     render: function(){
-        return (<PostingsListContainer criteria="all" page={this.props.routeParams.page} category={null} searchQuery={null}/>);
+        return (<PostingsListContainer criteria="all" page={this.props.routeParams.page ? (this.props.routeParams.page): "1"} category={null} searchQuery={null}/>);
     }
 });
 
 var CategoryWrapper = React.createClass({
     render: function(){
-        return (<PostingsListContainer criteria="category" page={this.props.routeParams.page} category={this.props.routeParams.category} search={null}/>);
+        return (<PostingsListContainer criteria="category" page={this.props.routeParams.page ? (this.props.routeParams.page): "1"} category={this.props.routeParams.category} search={null}/>);
     }
 });
 
 var SearchWrapper = React.createClass({
     render: function(){
-        return (<PostingsListContainer criteria="search" page={this.props.routeParams.page} search={this.props.routeParams.search} category={null}/>);
+        return (<PostingsListContainer criteria="search" page={this.props.routeParams.page ? (this.props.routeParams.page): "1"} search={this.props.routeParams.search} category={null}/>);
     }
 });
 
@@ -47,8 +47,11 @@ module.exports = (
     <Route path="/" component={Main}>
       <IndexRoute component={About} />
       <Route path="all/:page" component={AllWrapper}/>
+      <Route path="all" component={AllWrapper}/>
       <Route path="category/:category/:page" component={CategoryWrapper} />
+      <Route path="category/:category" component={CategoryWrapper}/>
       <Route path="search/:search/:page" component={SearchWrapper} />
+      <Route path="search/:search" component={SearchWrapper} />
       <Route path="question/:id" component={QuestionWrapper}/>
       <Route path="favorites" component={FavoriteWrapper}/>
       <Route path="*" status={404} component={BadLink}/>
