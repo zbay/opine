17ec99bdf167e76171eba31db259ae3924d9c471
zbay <zlwilliams1@gmail.com>
2016-04-04 00:19:22 +0000

    Now using babel to transform to ES5. Should fix most browser errors

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify');
 var exec = require('child_process').exec;
+var babelify = require('babelify');
 
 gulp.task('default', function() {
   var bundler = watchify(browserify({
     entries: ['./src/App.jsx'],
-    transform: [reactify],
     extensions: ['.jsx', '.scss'],
     debug: true,
     cache: {},
@@ -25,6 +25,7 @@ gulp.src('src/scss/*.scss')
   function build(file) {
     if (file) gutil.log('Recompiling ' + file);
     return bundler
+      .transform("babelify", {presets: ["es2015", "react"]})
       .bundle()
       .on('error', gutil.log.bind(gutil, 'Browserify Error'))
       .pipe(source('public/js/main.js'))
