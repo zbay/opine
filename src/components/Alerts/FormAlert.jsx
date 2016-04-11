a7182d22600c239d54f7e2704b23966d9296dce5
zbay <zlwilliams1@gmail.com>
2016-04-11 01:55:37 +0000

    Still trying to solve Heroku URL problem

--- src/components/Alerts/FormAlert.jsx
+++ src/components/Alerts/FormAlert.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
+var Link = require('react-router').Link;
 
 module.exports = React.createClass({
     render: function(){
         return (
         <div id="formAlert">
-        {this.props.successMessage ? (<div id="formSuccess">{this.props.successMessage}&nbsp;&nbsp;<a href={this.props.newPostURL}>View it now.</a></div>): (<span></span>)}
+        {this.props.successMessage ? (<div id="formSuccess">{this.props.successMessage}&nbsp;&nbsp;<Link to={this.props.newPostURL}>View it now.</Link></div>): (<span></span>)}
         {this.props.errorMessage ? ( <div id="formError">{this.props.errorMessage}</div>): (<span></span>)}
         </div>);
     }
