e0b68b6c5fa856f6e5ff87dfd3ae27cbe321446e
zbay <zlwilliams1@gmail.com>
2016-05-26 19:25:01 +0000

    Changed env variable name

--- server.js
+++ server.js
@@ -9,12 +9,9 @@ var db = mongoose.connection;
 var mongo = require('mongodb');
 var session = require('client-sessions');
 
-
-//https://ifelse.io/2015/08/27/server-side-rendering-with-react-and-react-router/ try this
-
 var dotenv = require('dotenv').load();
 //mongoose.connect('mongodb://localhost:27017/opine', function (err, db)
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/opine', function (err, db)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/opine', function (err, db)
 {
  if (err) {
       throw new Error('Database failed to connect!');
